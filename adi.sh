9e9dfd3d89c3472d9e8097dbce7f0e9bed27cd4b
TeknisiIT1 <139561646+TeknisiIT1@users.noreply.github.com>
2023-11-26 10:44:20 +0700

    Update adi.sh

--- adi.sh
+++ adi.sh
@@ -9,7 +9,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(curl -4 -k -sS ip.sb);
 echo "Authentikasi pada server"
-IZIN=$( curl -s https://raw.githubusercontent.com/adisubagja/AutoScriptXray/master/register.txt | grep -w $MYIP )
+IZIN=$( curl -s https://raw.githubusercontent.com/TeknisiIT1/KIKO1/main/register.txt | grep -w $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permintaan Diterima...${NC}"
 else
