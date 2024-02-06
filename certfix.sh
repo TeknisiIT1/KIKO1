dd87594a0a6a2717dc780736e2fd19e893ffaa95
TeknisiIT1 <139561646+TeknisiIT1@users.noreply.github.com>
2024-02-06 16:25:36 +0700

    Update certfix.sh

--- certfix.sh
+++ certfix.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl -s https://raw.githubusercontent.com/TeknisiIT1/izinkiko2/main/ip | grep $MYIP )
+IZIN=$( curl -s https://raw.githubusercontent.com/TeknisiIT1/KIKO1/main/register.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
