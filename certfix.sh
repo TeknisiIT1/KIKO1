02053e2f1a7c672570bd2ede861e912bceb876eb
TeknisiIT1 <139561646+TeknisiIT1@users.noreply.github.com>
2024-02-06 15:53:02 +0700

    Initial commit

---
+++ certfix.sh
@@ -0,0 +1,50 @@
+#!/bin/bash                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         #!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl -s https://raw.githubusercontent.com/TeknisiIT1/izinkiko2/main/ip | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+
+# PKG
+apt update && upgrade
+apt install git -y
+
+# Remove Necesarry Files
+rm -f /etc/adi/adi.crt
+rm -f /etc/adi/adi.key
+rm -f /etc/adi/domain
+rm -f /root/domain
+
+# Command
+echo "Masukan Domain Baru Kamu"
+read -p "Hostname / Domain: " host
+echo "$host" >> /etc/adi/domain
+echo "$host" >> /root/domain
+
+# ENV
+domain=$(cat /root/domain)
+
+# Clone Acme
+git clone https://github.com/acmesh-official/acme.sh.git /etc/acme
+cd /etc/acme
+systemctl stop nginx
+systemctl stop xray@vmesswsnontls
+chmod +x acme.sh
+./acme.sh --register-account -m netz@$domain
+./acme.sh --issue -d $domain --standalone
+./acme.sh --installcert -d $domain --key-file /etc/adi/adi.key --fullchain-file /etc/adi/adi.crt
+
+# Restart Service
+rm -f /root/domain
+systemctl restart nginx
+systemctl restart xray@vmesswsnontls
+echo "Domain Telah Diperbarui & Sertifikasi Selesai"
---
+++ oldadi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "Harap Menggunakan User ROOT"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "VPS OpenVZ Tidak Didukung"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Authentikasi pada server"
+IZIN=$( curl -s https://raw.githubusercontent.com/TeknisiIT1/izinkiko2/main/ip | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permintaan Diterima...${NC}"
+else
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
+rm -f adi.sh
+exit 0
+fi
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl wget && wget -q https://tunnel1.networkcloud.my.id/setup.sh && chmod +x setup.sh && ./setup.sh
